19a04976476ff5fbf999fe5c774ec3fba5a62d0f
Imparableub1 <ubongakpan@gmail.com>
2023-06-20 19:30:59 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-* main - print _putchar
-* is written by ubong
-* Return (0)
-*/
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
 
 int main(void)
 {
@@ -16,5 +17,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0)
+return (0);
 }
